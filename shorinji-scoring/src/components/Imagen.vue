3de32dfdd3f707b6e63a758a30953894c7f6edee
Stepha-360 <stepha02.sbv@gmail.com>
2026-02-26 12:57:05 -0500

    Enhance Imagen component with full-screen background
    Updated the Imagen component to include a full-screen background image with fixed positioning.

--- shorinji-scoring/src/components/Imagen.vue
+++ shorinji-scoring/src/components/Imagen.vue
@@ -1,15 +1,26 @@
 <script setup>
+
 import fondo from '@/assets/shorinji-fondo.png'
 </script>
 
 <template>
-  <div
-    class="columna"
+   <div
+    class="fondo"
     :style="{ backgroundImage: `url(${fondo})` }"
-  >
-  </div>
+  ></div>
 </template>
 
 <style scoped>
+ .fondo {
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: fixed;
+  inset: 0;
+  z-index: -1;
+}
+
 
 </style>
